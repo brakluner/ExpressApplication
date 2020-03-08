c3353e6c9aca845451b417e579efcaae90cf90e9
Ted Blount <52024892+brakluner@users.noreply.github.com>
2020-03-07 20:10:46 -0600

    so far so good
    I got the get and post functions working well. Now I need to get the DELETE function to work and then to see if I can get the list to render more correctly

---
+++ server.js
@@ -0,0 +1,81 @@
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+
+var app = express();
+var PORT = 4400;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + '/public')); 
+
+// var Notes = [
+//     {
+//       NoteTitle: "Christina",
+//       NoteText: "Chrissy"
+//     },
+//     {
+//       NoteTitle: "Ricci",
+//       NoteText: "Wednesday"
+//     },
+//     {
+//       NoteTitle: "Raul",
+//       NoteText: "Gomez"
+//     },
+//   ];
+
+let rawdata = fs.readFileSync('db.json');
+let Notes = JSON.parse(rawdata);
+var NotesData = require("./db.json")
+console.log(Notes);
+console.log(NotesData);
+
+//   fs.readFile(__dirname + "/../db/db.json", function(err, data) {
+//     if (err) throw err;
+    
+//     res.writeHead(200, { "Content-Type": "json" });
+//     res.end(data);
+//   });
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+  
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+  });
+  
+  app.get('/index.js', function (req, res) {
+    res.sendFile(path.join(__dirname, 'assets/js/index.js'));
+});
+
+app.get("/api/Note", function(req, res) {
+    res.json(NotesData);
+  });
+
+app.post("/api/Note", function(req, res) {
+    
+    var newNote = req.body;
+
+    console.log(newNote);
+  
+    // We then add the json the user sent to the character array
+    NotesData.push(newNote);
+  
+    // We then display the JSON to the users
+    res.json(newNote);
+  });
+
+app.delete('/api/Note', function (req, res) {
+  res.send('DELETE request to homepage')
+})
+  
+    
+    
+
+  
+
+  
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
